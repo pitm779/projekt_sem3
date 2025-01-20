b21d67f7aebf28a190ea3b32ebdd5cc7776e8aeb
pawelkarwecki <pkarwecki04@gmail.com>
2025-01-20 23:34:57 +0100

    Projekt wersja 1.0

--- wszystko.py
+++ wszystko.py
@@ -5,8 +5,3 @@ subprocess.run(['python', 'tabele.py']) #tworzenie pustych tabel
 subprocess.run(['python', 'category_fill.py']) #wypełnanie tabeli kategorii
 subprocess.run(['python', 'database_filler_template.py']) #wypełnianie pozostałych tabel
 
-#kompilacja raportu
-#try:
-    #subprocess.run(['Rscript', '-e', 'rmarkdown::render("raport_team03.Rmd")'])
-#except:
-    #pass
